92b6fc1858d7fa56d59dc7237edc133fd752024f
Rostyslav Kamach <ros.kamach@gmail.com>
2019-09-11 19:27:11 +0300

    Modify bash script and add Timeout:

--- project.sh
+++ project.sh
@@ -1,19 +1,35 @@
+#!/bin/bash
+LIGHT_GREAN='\033[1;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 LOGIN=system:admin
 SERVER_IP=$(minishift ip)
 
-
+if [ "$1" == "apply" ]
+    then
+        TIME=20
+    else
+        TIME=0
+fi
 oc login -u $LOGIN > /dev/null
-echo "################"
-echo Logged as $LOGIN
-echo "################"
-
+printf "${RED}################${NC}\n"
+printf "Logged as ${LIGHT_GREAN}$LOGIN${NC}\n"
+printf "${RED}################\n"
 oc process -f https://raw.githubusercontent.com/ros-kamach/openshift/master/rbac.yaml | oc $1 -f -
+printf "${RED}Sleep for ${TIME}sec${NC}\n"
+sleep $TIME
 oc process -f https://raw.githubusercontent.com/ros-kamach/openshift/master/jenkins/jenkins_persistent.yaml  | oc $1 -f -
+printf "${RED}Sleep for ${TIME}sec${NC}\n"
+sleep $TIME
 oc process -f https://raw.githubusercontent.com/ros-kamach/openshift/master/thunder/thunder_build.yaml  | oc $1 -f -
+printf "${RED}Sleep for ${TIME}sec${NC}\n"
+sleep $TIME
 oc process -f https://raw.githubusercontent.com/ros-kamach/openshift/master/thunder/thunder_deploy.yaml  | oc $1 -f -
+printf "${RED}Sleep for ${TIME}sec${NC}\n"
+sleep $TIME
 oc process -f https://raw.githubusercontent.com/ros-kamach/openshift/master/thunder/thunder_mysql.yaml  | oc $1 -f -
 
-echo "################"
-echo "The server is accessible via web console at:"
-echo "https://${SERVER_IP}:8443/console"
-echo "################"
+printf "${RED}################\n"
+printf "${LIGHT_GREAN}The server is accessible via web console at:${NC}\n"
+printf "${LIGHT_GREAN}https://${SERVER_IP}:8443/console${NC}\n"
+printf "${RED}################${NC}\n"

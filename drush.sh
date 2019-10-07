a4618bd6fd888fff360aaf90ef8c802060249a05
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-07 16:00:52 +0300

    Update drush.sh

--- drush.sh
+++ drush.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Add Enviroment to Progect "DRUSH_INSTALL=yes" to Install DRUSH
@@ -10,6 +9,18 @@ if [[ "${DRUSH_INSTALL}" == "yes" ]]
     else
         echo "###"
         echo  Skip DRUSH installation. Enviroment "DRUSH_INSTALL" is  $DRUSH_INSTALL, must be "yes"
+        echo "###"
+fi
+
+# Add Enviroment to Progect "PROMETHEUS_METRICS=yes" for Installing Prometheus Exporter
+if [[ "${PROMETHEUS_METRICS_INSTALL}" == "yes" ]]
+    then
+        echo "Installing Prometheus Exporter"
+        cd /usr/share/nginx/html/
+        composer require 'drupal/prometheus_exporter:1.x-dev'
+    else
+        echo "###"
+        echo  Skip Installing Prometheus Exporter. Enviroment "PROMETHEUS_METRICS_INSTALL" is  $PROMETHEUS_METRICS_INSTALL, must be "yes"
         echo "###"
 fi
 
@@ -36,5 +47,19 @@ if [[ "${SITE_INSTALL}" == "yes" ]]
     else
         echo "###"
         echo  Skip Install Site. Enviroment "SITE_INSTALL" is  $SITE_INSTALL, must be "yes"
+        echo "###"
+fi
+# Add Enviroment to Progect "ENABLE_METRICS=yes" for enable Prometheus Exporter
+if [[ "${ENABLE_METRICS}" == "yes" ]]
+    then
+        echo "Enable Prometheus Exporter"
+        cd /usr/share/nginx/html/
+        vendor/bin/drush en prometheus_exporter
+        sleep 20
+        vendor/bin/drush role-add-perm 'anonymous' 'access prometheus metrics'
+  
+    else
+        echo "###"
+        echo  Skip Installing Prometheus Exporter. Enviroment "ENABLE_METRICS" is  $ENABLE_METRICS, must be "yes"
         echo "###"
 fi
